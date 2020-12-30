109b6f209985e217dc17bfa14e6ee20648bc26b5
= <=>
2020-12-30 20:18:28 +0300

    control flow statements

--- script.js
+++ script.js
@@ -6,9 +6,28 @@ let personalMovieDB = {
     actorc: {},
     genres: [],
     privar: false
- };
-let lastFilm = prompt("Один из последних просмотренных фильмов?"),
-    markFilm = prompt("На сколько оцените его?");
-     
-personalMovieDB.movies[lastFilm] = markFilm;
+};
+
+for (let i = 0; i < 2; i++) {
+    const lastFilm = prompt("Один из последних просмотренных фильмов?"),
+        markFilm = prompt("На сколько оцените его?");
+
+    if (lastFilm != null && markFilm != null && lastFilm != '' && markFilm != '' &&
+        lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = markFilm;
+    } else {
+        i--;
+    }
+
+}
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов.");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель.");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман.");
+} else {
+    console.log("Произошла ошибка.");
+}
+
 console.log(personalMovieDB.movies);
