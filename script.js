91b2acc37d9078ea6d04246e4f522aa264e98a21
= <=>
2020-12-31 04:27:41 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,68 +1,76 @@
-let numberOfFilms;
 
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
 
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-    }
-}
-
-start();
 
 let personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actorc: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const lastFilm = prompt("Один из последних просмотренных фильмов?"),
+                markFilm = prompt("На сколько оцените его?");
+        
+            if (lastFilm != null && markFilm != null && lastFilm != '' && markFilm != '' &&
+                lastFilm.length < 50) {
+                personalMovieDB.movies[lastFilm] = markFilm;
+            } else {
+                i--;
+            }
+        
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов.");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+            console.log("Вы классический зритель.");
+        } else if (personalMovieDB.count > 30) {
+            console.log("Вы киноман.");
+        } else {
+            console.log("Произошла ошибка.");
+        }
+    },
+    showMyDB: function() {
+        if(personalMovieDB.privat == false) {
+            console.log("Ok");
+            console.log(personalMovieDB);
+        } else {
+            console.log("No access");
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 0; i < 3; i++) {
+            const answer = prompt(`Ваш любимый жанр под номером ${i+1}`, '');
+            if(answer != null && answer != "") {
+                personalMovieDB.genres[i] = answer;
+            } else {
+                i--;
+            }              
+        } 
+    },
+    toggleVisibleMyDB: function() {
+        if(personalMovieDB.privat == true) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+            
+    }
 };
 
+personalMovieDB.start();
+personalMovieDB.writeYourGenres();
+//personalMovieDB.showMyDB();
 
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const lastFilm = prompt("Один из последних просмотренных фильмов?"),
-            markFilm = prompt("На сколько оцените его?");
-    
-        if (lastFilm != null && markFilm != null && lastFilm != '' && markFilm != '' &&
-            lastFilm.length < 50) {
-            personalMovieDB.movies[lastFilm] = markFilm;
-        } else {
-            i--;
-        }
-    
-    }
-}
-
-rememberMyFilms();
-
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов.");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-        console.log("Вы классический зритель.");
-    } else if (personalMovieDB.count > 30) {
-        console.log("Вы киноман.");
-    } else {
-        console.log("Произошла ошибка.");
-    }
-}
-detectPersonalLevel();
-
-function showMyDB() {
-    if(personalMovieDB.privat == false) {
-        console.log(personalMovieDB);
-    }
-}
-
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        const answer = prompt(`Ваш любимый жанр под номером ${i+1}`);
-        personalMovieDB.genres[i] = answer;
-        
-    }
-}
-writeYourGenres();
-showMyDB();
+personalMovieDB.genres.forEach(function(key, i) {
+    console.log(`"Любимый жанр #${i+1} - это ${key}"`);
+});
